2b540a18cfd65b9cd80cb20cfa301ce8177e903a
mohammed-ashrf <greenbile2002@gmail.com>
2024-10-04 01:51:47 +0300

    adding messenger, instagram and line

--- src-tauri/gen/android/app/build.gradle.kts
+++ src-tauri/gen/android/app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
     defaultConfig {
         manifestPlaceholders["usesCleartextTraffic"] = "false"
         applicationId = "com.CipherSignal.dev"
-        minSdk= 34
+        minSdk= 24
         targetSdk = 34
         versionCode = tauriProperties.getProperty("tauri.android.versionCode", "1").toInt()
         versionName = tauriProperties.getProperty("tauri.android.versionName", "1.0")
